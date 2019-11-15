c35f694f3a48711c3382b8dd43b5a3df6078492a
Mitzi <mitzivera7@gmail.com>
2019-11-14 20:23:31 -0600

    changes

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-var express = require('express');
-var path = require('path');
+//Built in Node.js modules
 var fs = require('fs');
+var path = require('path');
 
-var app = express();
-var port= 8000;
-var public_dir = path.join(__dirname,'public');
+// NPM modules 
+var express = require('express');
+var sqlite3 = require('sqlite3');
+
+var public_dir = path.join(__dirname, 'public');
+var db_filename = path.join(__dirman, 'db', 'stpaul_crime.sqlite3');
+
+//opening the sqlite 3- the data 
+var db = new sqlite3.Database(db_filename,sqlite3.OPEN_READONLY, (err) =>{
+	if(err){
+		console.log("Error Opening "+ db_filename);
+	}else{
+		console.log("Now connected to "+ db_filename);
+	}
+});
+
+app.use (express.static(public_dir));
+
+//
+
 
